a16b0a528add65e7ab0a5be20c7b0927c2cdf90c
Moritz Kiefer <moritz.kiefer@digitalasset.com>
2019-03-25 14:44:45 +0100

    Prettier test name

--- preprocess.sh
+++ preprocess.sh
@@ -6,10 +6,13 @@ HS_FILE="$2"
 
 cp "$DAML_FILE" "$HS_FILE"
 sed -i -f replace.sed "$HS_FILE"
+MODULE_NAME="${HS_FILE#haskell/test/}"
+MODULE_NAME="${MODULE_NAME%.hs}"
+MODULE_NAME="${MODULE_NAME////.}"
 if grep 'scenario' "$HS_FILE" | grep -q -v "main" || [[ "$HS_FILE" == *Main.hs ]]; then
-    sed -i "s|main = scenario|main = describe \"${HS_FILE}\"|" "$HS_FILE"
+    sed -i "s|main = scenario|main = describe \"${MODULE_NAME}\"|" "$HS_FILE"
 else
-    sed -i "s|main = scenario|main = scenario \"${HS_FILE}\"|" "$HS_FILE"
+    sed -i "s|main = scenario|main = scenario \"${MODULE_NAME}\"|" "$HS_FILE"
 fi
 stack exec --stack-yaml with-preprocessor/stack.yaml with-preprocessor "$HS_FILE" "$HS_FILE"
 stack exec --stack-yaml with-preprocessor/stack.yaml record-dot-preprocessor "$HS_FILE" "$HS_FILE"
