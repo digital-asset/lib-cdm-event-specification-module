ab422e2ee1ddc87bb5f75be557c973944e52cc9d
Thomas Weber <thomas.weber@digitalasset.com>
2019-03-26 11:46:49 +0100

    Changed Folder structure

--- generate-haskell.sh
+++ generate-haskell.sh
@@ -4,12 +4,13 @@ set -euo pipefail
 stack build --stack-yaml with-preprocessor/stack.yaml
 stack build --stack-yaml with-preprocessor/stack.yaml record-dot-preprocessor
 
-for daml_file in $(find daml/ -name '*.daml'); do
+for daml_file in $(find daml/src/ -name '*.daml'); do
     hs_file="${daml_file%.daml}.hs"
-    if [[ "$daml_file" == daml//Test/* ]]; then
-        hs_file="haskell/test/${hs_file#daml/}"
+    echo $hs_file
+    if [[ "$daml_file" == daml/src//Test/* ]]; then
+        hs_file="haskell/test/${hs_file#daml/src/}"
     else
-        hs_file="haskell/src/${hs_file#daml/}"
+        hs_file="haskell/src/${hs_file#daml/src/}"
     fi
     mkdir -p "$(dirname $hs_file)"
     ./preprocess.sh "$daml_file" "$hs_file"
