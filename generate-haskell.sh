51a61b446af1ab625ab2177d3a4859f4aee8fcb5
Thomas Weber <thomas.weber@digitalasset.com>
2019-03-26 10:00:10 +0100

    Remove echo

--- generate-haskell.sh
+++ generate-haskell.sh
@@ -6,7 +6,6 @@ stack build --stack-yaml with-preprocessor/stack.yaml record-dot-preprocessor
 
 for daml_file in $(find daml/ -name '*.daml'); do
     hs_file="${daml_file%.daml}.hs"
-    echo $hs_file
     if [[ "$daml_file" == daml//Test/* ]]; then
         hs_file="haskell/test/${hs_file#daml/}"
     else
