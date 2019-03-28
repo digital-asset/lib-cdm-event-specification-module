ae460fbe0ab4cea8b85682d5cc54b73b1017daff
Thomas Weber <thomas.weber@digitalasset.com>
2019-03-28 11:45:26 +0100

    More documentation

---
+++ haskell/Readme.md
@@ -0,0 +1,10 @@
+# How to use
+
+Run the following command from the `scripts` folder to generate all Haskell files:
+
+    ./generate-haskell.sh
+
+
+In order to execute all tests, run:
+
+    stack test cdm:tests
--- haskell/scripts/generate-haskell.sh
+++ haskell/scripts/generate-haskell.sh
@@ -8,9 +8,13 @@ for daml_file in $(find ../../daml/src/ -name '*.daml'); do
     hs_file="${daml_file%.daml}.hs"
     if [[ "$daml_file" == ../../daml/src//Test/* ]]; then
         hs_file="../test/${hs_file#../../daml/src/}"
-    else
-        hs_file="../src/${hs_file#../../daml/src/}"
+        mkdir -p "$(dirname $hs_file)"
+        ./preprocess.sh "$daml_file" "$hs_file"
     fi
-    mkdir -p "$(dirname $hs_file)"
-    ./preprocess.sh "$daml_file" "$hs_file"
+
+    if [[ "$daml_file" == ../../daml/src//Org/* ]]; then
+        hs_file="../src/${hs_file#../../daml/src/}"
+        mkdir -p "$(dirname $hs_file)"
+        ./preprocess.sh "$daml_file" "$hs_file"
+    fi
 done
